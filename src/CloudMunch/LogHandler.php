ce375403bad2ec7f0f424158302b0f9527a1c8f4
rosmi <m>
2015-11-19 09:59:56 +0530

    Fix # LogHandler

---
+++ src/CloudMunch/LogHandler.php
@@ -0,0 +1,37 @@
+<?php
+namespace CloudMunch;
+class LogHandler{
+	private $appContext=null;
+	public function __construct($appContext){
+		$this->appContext = $appContext;
+		
+	}
+	function isdebugenabled(){
+		return true;
+	}
+	
+	function log($msgNo, $msg) {
+		try{
+			date_default_timezone_set('UTC');
+			$date = date(DATE_ATOM);
+		} catch (Exception $se) {
+		}
+		$stepname=$this->appContext->getStepName();
+		switch ($msgNo) {
+			case DEBUG :
+				if (isdebugenabled()) {
+					echo "<b>DEBUG</b> [$date][$stepname] $msg\n";
+				}
+				break;
+			case INFO :
+				echo "<b>INFO</b> [$date][$stepname] $msg\n";
+				break;
+			case ERROR:
+				echo "<b>ERROR</b> [$date] [$stepname]$msg\n";
+				break;
+					
+					
+		}
+	}
+	
+}
