a60a896d957879bbcade42c06c963d6a256fa3e9
syamk <syamk@cloudmunch.com>
2016-06-10 14:00:15 +0530

    Handle NTLM authentication
    If a username has \ in it, consider as a domain user and do
    low level NTML authentication for wsman

--- src/CloudMunch/libraries/powershell-interceptor.py
+++ src/CloudMunch/libraries/powershell-interceptor.py
@@ -42,25 +42,46 @@ else:
         script=myfile.read()
 script = script.decode('utf-8')
 
-
 for key,value in data.iteritems():
     if key:
         if key == "params":
             paramlist = value.split("--")
             for param in paramlist:
                 itemlist = param.split()
-                if 1 < len(itemlist): 
+                if 1 < len(itemlist):
                     script = script.replace("$$"+itemlist[0], itemlist[1])
         else:
             script = script.replace("$$"+key, value)
-script = "\n"+script            
+script = "\n"+script
 
-s = winrm.Session(data["servername"], auth=(data["serverusername"], data["password"]))
-r = s.run_ps(script)
+if "\\" in data["serverusername"]:
+    p = Protocol(
+        endpoint='http://'+data["servername"]+':5985/wsman',
+        transport='ntlm',
+        username=data["serverusername"],
+        password=data["password"],
+        server_cert_validation='ignore')
+    shell_id = p.open_shell()
+    command_id = p.run_command(shell_id, script)
+    std_out, std_err, status_code = p.get_command_output(shell_id, command_id)
+    p.cleanup_command(shell_id, command_id)
+    p.close_shell(shell_id)
+else:
+    s = winrm.Session(data["servername"], auth=(data["serverusername"], data["password"]))
+    try:
+        r = s.run_ps(script)
+    except winrm.exceptions.UnauthorizedError:
+        print "Could not authenticate on the remote server:", sys.exc_info()[1]
+        sys.exit(100)
+    except:
+        print "Unexpected error"
+        raise
 
-print r.std_out
+    std_out = r.std_out
+    std_err = r.std_err
+    status_code = r.status_code
 
-if r.std_err:
-    print r.std_err
+print std_out
 
-sys.exit(r.status_code)
+if std_err:
+    print std_err
